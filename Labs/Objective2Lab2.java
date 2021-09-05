263fa6f1f205c117c644f5ebfaba98cb81bc7aaf
CJ Harris <Carroll.Harris2@gmail.com>
2021-09-05 09:54:28 -0400

    printing multiple variables

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2{
+  public static void main(String [] args){
+    String name = "CJ Harris";
+    int age = 29;
+    String state = "Maryland";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
